12f1c771d74b1bd94a84b70fdd03ec3305a5a9c1
Antoine <antoine-rose@live.fr>
2018-04-24 22:31:39 +0200

    chore(compile.sh): improve the build script

--- compile.sh
+++ compile.sh
@@ -3,10 +3,10 @@
 PROGRAM_NAME="obs_test"
 SRC="main.cpp"
 CPP_FLAGS="-std=gnu++0x -DDL_OPENGL=\"libobs-opengl\""
-INCLUDE_PATH="obs-studio/libobs/"
+INCLUDE_PATH="-Iobs-studio/libobs/"
 LIBRARIES="-lobs"
 LIBRARIES_PATH="-Lobs-studio/build/libobs/"
 
-g++ ${CPP_FLAGS} -I${INCLUDE_PATH} ${SRC} ${LIBRARIES} ${LIBRARIES_PATH} -o ${PROGRAM_NAME}
-chmod +x obs_test
+g++ ${CPP_FLAGS} ${INCLUDE_PATH} ${SRC} ${LIBRARIES} ${LIBRARIES_PATH} -o ${PROGRAM_NAME}
+chmod +x ${PROGRAM_NAME}
 echo "Done"
