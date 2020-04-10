172e9ebe4e6e4ec7e6fc1e06b138bb0d3d0c6f48
Antoine Rose <antoou@gmail.com>
2020-04-10 20:46:55 +0200

    chore(compile.sh): improve build script with build dir and type

--- compile.sh
+++ compile.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-mkdir -p build
-cd build
-cmake ..
-make -j4
-echo -e "\n\n\033[32mDone, start with ./build/obs_headless\033[0m"
+BUILD_DIR="build"
+INSTALL_DIR=".."
+BUILD_TYPE=Release
+
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_INSTALL_RPATH="${BUILD_DIR}/lib"
+make -j4 && echo -e "\n\n\033[32mDone, start with ./build/obs_headless\033[0m"
