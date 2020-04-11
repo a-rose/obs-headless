0b5c27b822291f99d09fcd450eb32c7c26dfe735
Antoine Rose <antoine-rose@live.fr>
2020-04-11 19:47:50 +0200

    feat(compile.sh): use make -j nproc instead of defaulting to 4

--- compile.sh
+++ compile.sh
@@ -10,4 +10,4 @@ cd ${BUILD_DIR}
 cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
     -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_INSTALL_RPATH="${BUILD_DIR}/lib" \
     -DOBS_INSTALL_PATH="${OBS_INSTALL_PATH}"
-make -j4 && echo -e "\n\n\033[32mDone, start with ./build/obs_headless\033[0m"
+make -j$(nproc) && echo -e "\n\n\033[32mDone, start with ./build/obs_headless\033[0m"
