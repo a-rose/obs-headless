d96b9f09422935a01c40d39c5123df7f72509771
Antoine Rose <antoine-rose@live.fr>
2020-04-29 18:35:21 +0200

    feat(run.sh): print runtime directory

--- run.sh
+++ run.sh
@@ -3,7 +3,10 @@
 . config.sh
 
 here=$(pwd)
-cd ${OBS_INSTALL_PATH}/bin/64bit/
+cwd="${OBS_INSTALL_PATH}/bin/64bit/"
+cd $cwd
+
+echo "Current working directory (for core dumps etc): $cwd"
 
 if [ "$1" == "-g" ]; then
     gdb ${here}/build/obs_headless
